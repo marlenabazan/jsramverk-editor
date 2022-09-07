f044f0b3f55aa4ae2fa72ce46dcd998f3da5823b
Marlena Bazan <marlena.bazan@hotmail.com>
2022-09-07 16:29:26 +0200

    kmom01 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import "trix";
+import "trix/dist/trix.css";
+import { TrixEditor } from "react-trix";
+
+
 function App() {
+  const [text, setText] = useState("")
+  const [html, setHTML] = useState("")
+
+  const handleChange = (html, text) => {
+    setHTML(html)
+    setText(text)
+  }
+
+  const save = () => {
+    console.log(text)
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Text Editor
+        </h1>
       </header>
+
+      <div className="SaveDiv">
+        <button className="Save" onClick={save}>Save</button>
+      </div>
+
+      <div className="Editor">
+        <TrixEditor onChange={handleChange} placeholder="Write here..."/>
+      </div>
+
     </div>
   );
 }
+
 
 export default App;

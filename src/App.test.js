b95294d3dfc7d5ad00cc408a74454923a4327897
Marlena Bazan <marlena.bazan@hotmail.com>
2023-01-17 11:08:13 +0100

    Added some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders sites header', () => {
+  const { container } = render(<App />);
+
+  expect(screen.getByText(/Text Editor/i)).toBeInTheDocument();
+});
+
+test('renders create new document button', () => {
+  const { container } = render(<App />);
+
+  expect(screen.getByText(/Create new document/i)).toBeInTheDocument();
+});
+
+test('renders update document button', () => {
+  const { container } = render(<App />);
+
+  expect(screen.getByText(/Update a document/i)).toBeInTheDocument();
 });
